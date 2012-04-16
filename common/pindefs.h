4aff49dc2f7c5104f90ea22cc9479aaadc27613f
Alan Duncan <alan@cocoa-factory.com>
2012-04-15 20:17:37 -0500

    Added pindefs to globally keep track of pin definitions

---
+++ common/pindefs.h
@@ -0,0 +1,19 @@
+/*
+ * pindefs.h
+ *
+ * Created: 4/15/2012 6:17:20 AM
+ *  Author: Administrator
+ */ 
+
+
+#ifndef PINDEFS_H_
+#define PINDEFS_H_
+
+/*	BMP085 temperature and pressure sensor */
+
+#define BMP085_EOC_PORT	PORTC
+#define BMP085_EOC_PIN	PC0
+
+
+
+#endif /* PINDEFS_H_ */
