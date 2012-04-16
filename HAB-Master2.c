183240ecdaac0d19a6f3b9c0de0fe24fb16c1df6
Alan Duncan <alan@cocoa-factory.com>
2012-04-16 06:01:56 -0500

    added references to peripherals, break out init to separate function

--- HAB-Master2.c
+++ HAB-Master2.c
@@ -9,10 +9,17 @@
 #include <avr/io.h>
 #include "capabilities/i2c.h"
 #include "peripherals/ds1307.h"
+#include "peripherals/bmp085.h"
+#include "peripherals/tmp102.h"
+#include "peripherals/ds18x20.h"
 #include "common/global.h"
 #include "common/types.h"
 
 global_states_t global_states;
+
+/*	FUNCTION PROTOTYPES	*/
+
+void init(void);
 
 int main(void)
 {
@@ -25,4 +32,14 @@ int main(void)
     {
         //TODO:: Please write your application code 
     }
+}
+
+void init(void) {
+	//	start out in configuration state
+	global_states.system_state = k_system_state_configure;
+	//	our flight state is preflight
+	global_states.flight_state = k_state_preflight;
+	
+	i2cInit();							//	init I2C bus
+	ds1307_init(kDS1307Mode24HR);		//	init RTC
 }
