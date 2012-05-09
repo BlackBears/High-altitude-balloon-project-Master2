2fc77133ba639c57141212c387915b23d2489db4
Alan Duncan <alan@cocoa-factory.com>
2012-05-08 20:58:53 -0500

    remove logger.h/c

--- peripherals/logger.h
+++
@@ -1,7 +0,0 @@
-
-#include "../capabilities/fat/diskio.h"
-#include "../capabilities/fat/ff.h"
-#include "../common/global.h"
-
-/**	initializes the logger, returning TRUE if it succeeds */
-BOOL logger_init(void);
